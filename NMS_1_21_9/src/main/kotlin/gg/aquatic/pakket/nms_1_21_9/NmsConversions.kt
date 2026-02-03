5f28cf438618c1da70c65b176e5cb770179f451c
MrLarkyy_ <studiotadikl@gmail.com>
2026-02-03 23:32:56 +0100

    Simplify `toNmsComponent` conversion using `AdventureComponent` and clean up imports.

--- NMS_1_21_9/src/main/kotlin/gg/aquatic/pakket/nms_1_21_9/NmsConversions.kt
+++ NMS_1_21_9/src/main/kotlin/gg/aquatic/pakket/nms_1_21_9/NmsConversions.kt
@@ -1,21 +1,14 @@
 package gg.aquatic.pakket.nms_1_21_9
 
-import com.google.gson.JsonParser
+import io.papermc.paper.adventure.AdventureComponent
 import net.kyori.adventure.text.Component
-import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer
-import net.minecraft.network.chat.ComponentSerialization
-import com.mojang.serialization.JsonOps
 import org.bukkit.craftbukkit.inventory.CraftItemStack
 import org.bukkit.inventory.ItemStack
 
 object NmsConversions {
 
     fun toNmsComponent(component: Component): net.minecraft.network.chat.Component {
-        val kyoriJson = GsonComponentSerializer.gson().serialize(component)
-        return ComponentSerialization.CODEC.parse(
-            JsonOps.INSTANCE,
-            JsonParser.parseString(kyoriJson)
-        ).orThrow
+        return AdventureComponent(component)
     }
 
     fun toNmsItemStack(itemStack: ItemStack): net.minecraft.world.item.ItemStack {
